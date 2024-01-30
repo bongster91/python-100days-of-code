87ea0b11986ab4cc7375921e742e5414848b39ce
Grace Bong <gracebong.park@gmail.com>
2024-01-30 13:10:17 -0500

    tip calculator complete

---
+++ tip_calculator.py
@@ -0,0 +1,7 @@
+print('Welcome to the tip calculator.')
+total = input('What was the total bill? ')
+tip = input('What percentage tip would you like to give? 10, 12, or 15? ')
+people = input('How many people to split the bill? ')
+post_tax = float(total) * (1 / int(tip)) + float(total)
+share = round(post_tax / int(people), 2)
+print(f'Each person should pay: ${share}')
