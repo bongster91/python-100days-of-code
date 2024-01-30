0b435f88bc2193b1de1e5cde7265e62eeaf73e81
Grace Bong <gracebong.park@gmail.com>
2024-01-30 12:30:42 -0500

    band name generator complete

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print('Welcome to band name generator!')
+city = input('What city did you grow up in? \n')
+pet = input('What is your pet"s name? \n')
+print(f'Your band name could be: {city} {pet}')
