005c07660c69d490cfdecf3f5d178ce6b3ec6ef1
Grace Bong <gracebong.park@gmail.com>
2024-02-05 16:42:02 -0500

    pong complete

--- pong/main.py
+++ pong/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, 'Up')
@@ -23,7 +25,7 @@ screen.onkey(l_paddle.down, 's')
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     
@@ -34,5 +36,14 @@ while game_is_on:
     #Detect collision with right paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    
+    #Detect Left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+        
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
     
 screen.exitonclick()
