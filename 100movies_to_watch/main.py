27067b5f7afee98602ba31397f7e0b125bc94a42
Grace Bong <gracebong.park@gmail.com>
2024-02-11 22:10:21 -0500

    top movies list complete

---
+++ 100movies_to_watch/main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = 'https://empireonline.com/movies/features/best-movies-2'
+
+response = requests.get(URL)
+webpage = response.text
+
+# soup = BeautifulSoup(yc_webpage, 'html.parser')
+# article_tag = soup.find(name='span', class_='titleline')
+# article_text = article_tag.get_text()
+# article_link = article_tag('a')
+# article_upvote = soup.find(name='span', class_='score').getText()
+
+soup = BeautifulSoup(webpage, 'html.parser')
+movies_list = soup.find_all(name='h3', class_='listicleItem_listicle-item__title__BfenH')
+movies_list.reverse()
+movie_titles = [movie.getText() for movie in movies_list]
+
+with open('top-movies.txt', mode='w') as file:
+    for movie in movie_titles:
+        file.write(f"{movie}\n")
