b10e467e86101a42d0a319ba3b0120a892570bbb
Grace Bong <gracebong.park@gmail.com>
2024-02-15 17:27:24 -0500

    wip

--- flask_tutorial/main.py
+++ flask_tutorial/main.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+class PlayerCharacter:
+    def __init__(self, name) -> None:
+        self.name = name
+        
+    def run(self):
+        print('run')
+        
+player1 = PlayerCharacter()
